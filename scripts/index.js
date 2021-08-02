11638518df099459ae8f8f5942b1416474d7ff3e
Nick354 <80168096+Nick354@users.noreply.github.com>
2021-08-02 23:50:47 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-let popupButton = document.querySelector(".info__edit-button");
-let closeButton = document.querySelector(".popup__close");
-let popup = document.querySelector(".popup");
-let nameElement = document.querySelector(".info__title");
-let jobElement = document.querySelector(".info__subtitle");
+let popupButton = document.querySelector('.info__edit-button');
+let closeButton = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let nameElement = document.querySelector('.info__title');
+let jobElement = document.querySelector('.info__subtitle');
 // Находим форму в DOM
-let formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__input-name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__input-job"); // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('.popup__input_name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_job'); // Воспользуйтесь инструментом .querySelector()
 
 function openPopup() {
-  popup.classList.add("popup_opend");
+  popup.classList.add('popup_opend');
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
 }
 function closePopup() {
-  popup.classList.remove("popup_opend");
+  popup.classList.remove('popup_opend');
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -36,6 +36,6 @@ function formSubmitHandler(evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", formSubmitHandler);
-popupButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+popupButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
